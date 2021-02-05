be2cd6d0132148bc9295c1ae0b0ff15444e68831
JagaPrmdt <jagaparamudita09@gmail.com>
2021-02-05 13:42:20 +0700

    Intent [memberi tindakan pindah halaman]

---
+++ app/src/main/java/com/example/smpn18kotategal/ProfilActivity.java
@@ -0,0 +1,18 @@
+package com.example.smpn18kotategal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ProfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+    }
+
+
+}
