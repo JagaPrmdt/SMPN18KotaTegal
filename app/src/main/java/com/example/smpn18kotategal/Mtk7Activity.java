bffc70934da6bdfc8f006d540a1f85ef2e25da18
dimastriwahyudi <Dimastriwahyudi68@gmailcom>
2021-03-29 14:06:01 +0700

    menuperpus

--- app/src/main/java/com/example/smpn18kotategal/Mtk7Activity.java
+++ app/src/main/java/com/example/smpn18kotategal/Mtk7Activity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class Mtk7Activity extends AppCompatActivity {
+    private PDFView pdfView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mtk7);
+
+
+        pdfView =(PDFView) findViewById(R.id.pdfView);
+
+        pdfView.fromAsset("mtk7.pdf").load();
+
     }
 }
--- app/src/main/java/com/example/smpn18kotategal/PerpustakaanActivity.java
+++ app/src/main/java/com/example/smpn18kotategal/PerpustakaanActivity.java
@@ -2,7 +2,9 @@ package com.example.smpn18kotategal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PerpustakaanActivity extends AppCompatActivity {
 
@@ -11,5 +13,65 @@ public class PerpustakaanActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perpustakaan);
 
+
+
+
+
     }
+
+    public void mtk7(View view){
+        Intent intent=new Intent(PerpustakaanActivity.this, Mtk7Activity.class);
+        startActivity(intent);
+    }
+
+    public void bind7(View view){
+        Intent intent=new Intent(PerpustakaanActivity.this, Bind7Activity.class);
+        startActivity(intent);
+    }
+    public void bing7(View view){
+        Intent intent=new Intent(PerpustakaanActivity.this, Bing7Activity.class);
+        startActivity(intent);
+    }
+
+    public void ipa7(View view){
+        Intent intent=new Intent(PerpustakaanActivity.this, Ipa7Activity.class);
+        startActivity(intent);
+    }
+
+    public void ips7(View view){
+        Intent intent=new Intent(PerpustakaanActivity.this, Ips7Activity.class);
+        startActivity(intent);
+    }
+
+    public void pai7(View view){
+        Intent intent=new Intent(PerpustakaanActivity.this, Pai7Activity.class);
+        startActivity(intent);
+    }
+
+    public void pjok7(View view){
+        Intent intent=new Intent(PerpustakaanActivity.this, Pjok7Activity.class);
+        startActivity(intent);
+    }
+
+    public void pkn7(View view){
+        Intent intent=new Intent(PerpustakaanActivity.this, Pkn7Activity.class);
+        startActivity(intent);
+    }
+
+    public void prakarya7(View view){
+        Intent intent=new Intent(PerpustakaanActivity.this, Prakarya7Activity.class);
+        startActivity(intent);
+    }
+
+    public void sby7(View view){
+        Intent intent=new Intent(PerpustakaanActivity.this, Sby7Activity.class);
+        startActivity(intent);
+    }
+
+    public void bjawa7(View view){
+        Intent intent=new Intent(PerpustakaanActivity.this, Bjawa7Activity.class);
+        startActivity(intent);
+    }
+
+
 }
