2aa4ed73033830b3023aad06be964f8201532945
JagaPrmdt <jagaparamudita09@gmail.com>
2021-04-05 14:09:33 +0700

    penambahan menu perpus 1

--- app/src/main/java/com/example/smpn18kotategal/EkstrakulikulerActivity.java
+++ app/src/main/java/com/example/smpn18kotategal/EkstrakulikulerActivity.java
@@ -14,22 +14,22 @@ public class EkstrakulikulerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ekstrakulikuler);
     }
 
-    public void osis(View view){
+    public void osis1(View view){
         Intent intent=new Intent(EkstrakulikulerActivity.this, OsisActivity.class);
         startActivity(intent);
     }
 
-    public void pramuka(View view){
+    public void pramuka2(View view){
         Intent intent=new Intent(EkstrakulikulerActivity.this,PramukaActivity.class);
         startActivity(intent);
     }
 
-    public void pmr(View view){
+    public void pmr3(View view){
         Intent intent=new Intent(EkstrakulikulerActivity.this,PmrActivity.class);
         startActivity(intent);
     }
 
-    public void futsal(View view){
+    public void futsal4(View view){
         Intent intent=new Intent(EkstrakulikulerActivity.this, FutsalActivity.class);
         startActivity(intent);
     }
